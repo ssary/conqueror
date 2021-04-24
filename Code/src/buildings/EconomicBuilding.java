5e94e046cc2394bdd04925c056a8cf51903d962a
sary nasser <53094371+ssary@users.noreply.github.com>
2021-04-24 16:49:44 +0200

    Add files via upload

---
+++ Code/src/buildings/EconomicBuilding.java
@@ -0,0 +1,10 @@
+package buildings;
+
+abstract public class EconomicBuilding extends Building{
+	public EconomicBuilding() {
+		
+	}
+	public EconomicBuilding(int cost,int upgradeCost) {
+		super(cost , upgradeCost);
+	}
+}
---
+++ Code/src/buildings/Stable.java
@@ -0,0 +1,6 @@
+package buildings;
+
+public class Stable extends MilitaryBuilding{
+	public Stable() {
+	super(2500 , 1500 , 600);}
+}
